c6ed3331e558dabcbd8a33aa5f2cbaf874e63b6e
Max Gomes <maxgomes92@live.com>
2018-07-02 22:26:05 -0300

    Add parse installation save

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Parse from 'parse';
+import Home from "./components/home";
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // TODO don't forget to add your app and js ids
+    Parse.initialize("YOUR-APP-ID", "YOUR-JS-KEY");
+    Parse.serverURL = 'https://parseapi.back4app.com/';
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Back4App Tutorial</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Home/>
       </div>
     );
   }
